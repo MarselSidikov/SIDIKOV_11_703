b985e4406e6ff1c451cfa06fb51d7f8d37111082
MarselSidikov <sidikov.marsel@gmail.com>
2018-03-15 09:37:23 +0300

    add maps

---
+++ Themes/Maps/src/ru/itis/HashMapImpl.java
@@ -0,0 +1,29 @@
+package ru.itis;
+
+/**
+ * 15.03.2018
+ * HashMapImpl
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+public class HashMapImpl<K, V> implements Map<K, V> {
+
+    private V map[];
+
+    public HashMapImpl() {
+        map = (V[])new Object[10];
+    }
+
+    @Override
+    public void put(K key, V value) {
+        int position = Math.abs(key.hashCode() % 10);
+        map[position] = value;
+    }
+
+    @Override
+    public V get(K key) {
+        int position = Math.abs(key.hashCode() % 10);
+        return map[position];
+    }
+}
