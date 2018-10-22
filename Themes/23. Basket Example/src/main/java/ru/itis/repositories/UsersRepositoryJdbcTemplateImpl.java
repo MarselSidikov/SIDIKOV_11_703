b0e167311784d171e6303dbf5406096a06ebdd49
MarselSidikov <sidikov.marsel@gmail.com>
2018-10-22 17:19:43 +0300

    add basket

---
+++ Themes/23. Basket Example/src/main/java/ru/itis/repositories/UsersRepositoryJdbcTemplateImpl.java
@@ -0,0 +1,67 @@
+package ru.itis.repositories;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import ru.itis.models.User;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * 22.10.2018
+ * UsersRepositoryJdbcTemplateImpl
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+public class UsersRepositoryJdbcTemplateImpl implements UsersRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    //language=SQL
+    private static final String SQL_SELECT_USER_BY_ID =
+            "select * from shop_user where id = ?";
+
+    //language=SQL
+    private static final String SQL_SELECT_ALL_USERS =
+            "select * from shop_user";
+
+    private RowMapper<User> userRowMapper = (resultSet, i) -> {
+        return User.builder()
+                .id(resultSet.getLong("id"))
+                .name(resultSet.getString("name"))
+                .build();
+    };
+
+    public UsersRepositoryJdbcTemplateImpl(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return jdbcTemplate.query(SQL_SELECT_ALL_USERS, userRowMapper);
+    }
+
+    @Override
+    public User find(Long id) {
+        return jdbcTemplate.queryForObject(SQL_SELECT_USER_BY_ID,
+                userRowMapper, new Object[]{id});
+    }
+
+    @Override
+    public void save(User model) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public void update(User model) {
+
+    }
+}
