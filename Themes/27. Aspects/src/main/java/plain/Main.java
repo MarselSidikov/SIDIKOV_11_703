e481bb4f30e2c86b9b87804d6a11cea10827ef19
MarselSidikov <sidikov.marsel@gmail.com>
2019-02-21 11:13:19 +0300

    aspects

---
+++ Themes/27. Aspects/src/main/java/plain/Main.java
@@ -0,0 +1,22 @@
+package plain;
+
+/**
+ * 27.02.2018
+ * plain.Main
+ *
+ * @author Sidikov Marsel (First Software Engineering Platform)
+ * @version v1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        CarProxy proxy = new CarProxy(car,
+                () -> System.out.println("Run before"),
+                () -> System.out.println("Run after"),
+                () -> System.out.println("Я не буду работать"));
+
+        Driver driver = new Driver(proxy);
+
+        driver.drive();
+    }
+}
