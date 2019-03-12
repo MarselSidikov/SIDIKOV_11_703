ecd68bef6f1d163d8c466bf40fc6566085dd6ba6
MarselSidikov <sidikov.marsel@gmail.com>
2019-03-12 15:06:56 +0300

    add new mvc

--- Themes/23. Basket Example/src/main/java/ru/itis/controllers/UsersController.java
+++ Themes/23. Basket Example/src/main/java/ru/itis/controllers/UsersController.java
@@ -1,10 +1,13 @@
 package ru.itis.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 import ru.itis.services.UsersService;
 import ru.itis.models.User;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -16,17 +19,18 @@ import java.util.List;
  * @author Sidikov Marsel (First Software Engineering Platform)
  * @version v1.0
  */
-public class UsersController implements Controller {
+@Controller
+public class UsersController {
 
     @Autowired
     private UsersService usersService;
 
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public ModelAndView getUsersPage() {
         List<User> users = usersService.getAllUsers();
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("users", users);
-        modelAndView.setViewName("users");
+        modelAndView.setViewName("users_page");
         return modelAndView;
     }
 }
